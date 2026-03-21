44900ed16ba9cd20337d4feec4086eecbef9b8e5
Lucas Silva e Pereira <lucaspereira.dev@proton.me>
2026-03-21 00:16:07 -0300

    feat: Configure Pool Connection

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,7 +3,12 @@ import { env } from "env.js";
 import { PrismaClient } from "../generated/prisma/client.js";
 
 const adapter = new PrismaNeon(
-	{ connectionString: env.DATABASE_URL },
+	{
+		connectionString: env.DATABASE_URL,
+		max: 100,
+		connectionTimeoutMillis: 2_000,
+		idleTimeoutMillis: 2_000,
+	},
 	{ schema: "mySchema" },
 );
 export const prisma = new PrismaClient({ adapter });
